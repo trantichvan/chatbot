86eabd9ca932fa22de2495c9d7888a913f9033b3
trantichvan <hoangtubongdemx19@gmail.com>
2017-05-20 18:58:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId:'',// process.env.MICROqT_APP_ID,
-    appPassword:'' //process.env.MICROSOFT_APP_PASSWORD
+    appId:'31c6acd6-30e3-4d5d-a3df-8de1a74c24b7',// process.env.MICROqT_APP_ID,
+    appPassword:'GLvgXeigv29hr3fuNqxkKdV' //process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
